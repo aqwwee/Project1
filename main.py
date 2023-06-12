eb7009d520316bd1af5240669a3968d9c73a7035
JeonSeungWon <jsw1255@gmail.com>
2023-06-12 17:39:31 +0900

    커밋5 제곱함수 추가

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def mul_func(n1,n2):
 def div_func(n1,n2):
     return n1/n2
 
+def pow_func(n1,n2):
+    return n1**n2
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -26,3 +29,6 @@ print(num1,"*",num2,"=",res)
 
 res = div_func(num1,num2)
 print(num1,"/",num2,"=",res)
+
+res = pow_func(num1,num2)
+print(num1,"**",num2,"=",res)
